86ab5baa68ee75fb17a7e0f0bd6eb19e6b0ac154
amds <450076077@qq.com>
2017-12-01 10:46:52 +0800

    add

--- generate_beam_viz.py
+++ generate_beam_viz.py
@@ -19,6 +19,10 @@
 """ Generate beam search visualization.
 """
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
